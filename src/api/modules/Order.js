9e047648f19292197f7241936129fdc019e4dd2f
shentong <shentong@meishubao.com>
2020-03-13 19:44:12 +0800

    feat(): init-axios

---
+++ src/api/modules/Order.js
@@ -0,0 +1,22 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: shentong
+ * @Date: 2020-03-13 16:20:48
+ * @LastEditors: shentong
+ * @LastEditTime: 2020-03-13 18:58:56
+ */
+import axios from '../axios'
+
+export default {
+  /**
+   * 订单列表
+   * */
+  loginIn(params) {
+    return axios.post('/api/admin/login/pass222', params)
+  },
+  orderList(params) {
+    // return this.post(this.API_HOST + '/admin/login/pass/2', params)
+    return axios.post('/data/orderList', params)
+  }
+}
