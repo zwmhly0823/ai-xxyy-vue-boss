26d6a15eb96b78abecf20ed4c85c52d1b8bb29c0
panjian <panjian@meishubao.com>
2020-03-16 18:13:54 +0800

    feat: 带班详情

--- src/pages/studentTeam/components/DetailsTab.vue
+++ src/pages/studentTeam/components/DetailsTab.vue
@@ -1,11 +1,256 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: panjian
+ * @Date: 2020-03-16 14:19:58
+ * @LastEditors: panjian
+ * @LastEditTime: 2020-03-16 18:02:11
+ -->
 <template>
-  <div>带班详情</div>
+  <div>
+    <div>
+      <el-button size="small" @click="enter('1')" round>加好友进群</el-button>
+      <el-button size="small" round>物流</el-button>
+      <el-button size="small" round>登陆</el-button>
+      <el-button size="small" round>参课</el-button>
+      <el-button size="small" round>完课</el-button>
+      <el-button size="small" round>传作品</el-button>
+      <el-button size="small" round>点评</el-button>
+      <el-input
+        class="el-input-search"
+        size="mini"
+        placeholder="昵称、手机号、微信信息"
+        prefix-icon="el-icon-search"
+        v-model="input"
+        @keyup.enter.native="enter"
+      >
+      </el-input>
+    </div>
+    <div>
+      <el-table
+        height="350"
+        class="el-table-list"
+        ref="multipleTable"
+        :data="tableData"
+        tooltip-effect="dark"
+        style="width: 100%"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55"> </el-table-column>
+        <el-table-column label="日期" width="120">
+          <template slot-scope="scope">{{ scope.row.date }}</template>
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120">
+        </el-table-column>
+        <el-table-column prop="address" label="地址" show-overflow-tooltip>
+        </el-table-column>
+      </el-table>
+      <div class="table-flex">
+        <span
+          >当前结果：共计{{ student }}个学员，{{ friend }}已加好友，{{
+            group
+          }}已进微信群</span
+        >
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page.sync="currentPage3"
+          :page-size="100"
+          layout="prev, pager, next, jumper"
+          :total="1000"
+        >
+        </el-pagination>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      group: '10',
+      friend: '30',
+      student: '60',
+      input: '',
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }
+      ],
+      multipleSelection: [],
+      currentPage3: 1
+    }
+  },
+  mounted() {},
+  methods: {
+    enter(val) {
+      console.log(this.input, 'input', val)
+    },
+    handleSelectionChange(val) {
+      console.log(val, 'val')
+      this.multipleSelection = val
+    },
+    // 分页
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+    }
   }
 }
 </script>
-<style scoped></style>
+<style scoped>
+.table-flex {
+  display: flex;
+  /* justify-content: space-between; */
+  align-items: center;
+}
+.el-input-search {
+  float: right;
+  width: 180px;
+}
+.el-button--default {
+  border-radius: 15px;
+}
+.div {
+  padding-top: 20px;
+}
+</style>
