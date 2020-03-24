10ba86632fefd4f97372d3b12ec137f9b065f900
System Administrator <root@chengweihua.local>
2020-03-24 17:34:11 +0800

    fix: 订单列表布局

---
+++ src/pages/trading/views/order/components/allOrder.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <article class="top-box">
+      <!-- <div class="total-orider">1</div>
+      <div class="experience-orider">2</div>
+      <div class="system-orider">3</div>
+      <div class="bear-orider">4</div>
+      <div class="recommended-order">5</div> -->
+      <el-row :gutter="20">
+        <el-col :span="8"><div class="grid-content bg-purple">1</div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple">2</div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple">3</div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple">4</div></el-col>
+        <el-col :span="4"><div class="grid-content bg-purple">5</div></el-col>
+      </el-row>
+    </article>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.top-box {
+  width: 100%;
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+}
+</style>
--- src/pages/trading/views/order/orderModule.vue
+++ src/pages/trading/views/order/orderModule.vue
@@ -1,35 +1,44 @@
 <!--
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: zhubaodong
  * @Date: 2020-03-24 15:16:26
  * @LastEditors: zhubaodong
  * @LastEditTime: 2020-03-24 16:20:20
  -->
 <template>
-  <div class="">
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
-    <div>1</div>
+  <div class="order-box">
+    <el-tabs type="border-card" @tab-click="handleClick">
+      <el-tab-pane label="全部订单">
+        <all-order />
+      </el-tab-pane>
+      <el-tab-pane label="代支付">代支付</el-tab-pane>
+      <el-tab-pane label="已支付">已支付</el-tab-pane>
+      <el-tab-pane label="已完成">已完成</el-tab-pane>
+      <el-tab-pane label="退费中">退费中</el-tab-pane>
+      <el-tab-pane label="已退费">已退费</el-tab-pane>
+      <el-tab-pane label="已关闭">已关闭</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
+import allOrder from './components/allOrder'
 export default {
   props: [],
-  components: {},
+  components: {
+    allOrder
+  },
   data() {
     return {}
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab.index, event)
+    }
+  },
   created() {},
   mounted() {}
 }
