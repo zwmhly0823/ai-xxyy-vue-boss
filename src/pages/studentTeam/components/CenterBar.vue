f0388dbc3c81f830e40a341c50e67f0910e9357e
zbd <zz1050729352@outlook.com>
2020-03-24 22:40:00 +0800

    fix: 下拉刷新

--- src/pages/studentTeam/components/CenterBar.vue
+++ src/pages/studentTeam/components/CenterBar.vue
@@ -4,7 +4,7 @@
  * @Author: zhubaodong
  * @Date: 2020-03-13 16:53:33
  * @LastEditors: zhubaodong
- * @LastEditTime: 2020-03-24 22:07:58
+ * @LastEditTime: 2020-03-24 22:39:33
  -->
 <template>
   <div class="center-container">
@@ -151,7 +151,7 @@ export default {
       console.log('触发load函数')
       // 页码总数大于20 并且当前页数小于总页数
       if (
-        +this.showClassData.pageData.totalElements > 20 &&
+        +this.showClassData.pageData.totalElements > 15 &&
         +this.showClassData.pageData.totalPages >
           this.classData.teamStatusPage.number
       ) {
