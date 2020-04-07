31a125940766db3a107a9beb64b7fdc6c7a6face
zbd <zz1050729352@outlook.com>
2020-04-07 14:39:46 +0800

    update: 左侧组织结构数据调试

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@
  * @Author: shentong
  * @Date: 2020-03-13 14:38:28
  * @LastEditors: zhubaodong
- * @LastEditTime: 2020-03-30 21:09:50
+ * @LastEditTime: 2020-04-07 13:57:34
  */
 import Login from './modules/Login'
 import User from './modules/User' // 用户接口
@@ -12,6 +12,7 @@ import Order from './modules/Order' // 订单接口
 import Team from './modules/Team' // 班级接口
 import Express from './modules/Express' // 物流接口
 import StudentTerm from './modules/StudentTerm' // 班级graphql接口
+import Teacher from './modules/teacher' // 老师模块接口
 
 export default Object.assign(
   {},
@@ -20,5 +21,6 @@ export default Object.assign(
   { User },
   { Team },
   { StudentTerm },
-  { Express }
+  { Express },
+  { Teacher }
 )
