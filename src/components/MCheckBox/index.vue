71fe245717739cee167926df5769fc1773946371
panjian <panjian@meishubao.com>
2020-04-07 21:13:55 +0800

    fix: 修改图片大小问题

--- src/components/MCheckBox/index.vue
+++ src/components/MCheckBox/index.vue
@@ -4,80 +4,180 @@
  * @Author: panjian
  * @Date: 2020-03-16 20:22:24
  * @LastEditors: panjian
- * @LastEditTime: 2020-04-07 20:12:29
+ * @LastEditTime: 2020-04-07 20:53:53
  -->
 <template>
   <div>
-    <!-- <svg
-      t="1586260816244"
-      class="icon"
-      viewBox="0 0 1024 1024"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      p-id="1148"
-      width="200"
-      height="200"
-    >
-      <path
-        d="M859.02 234.524l0.808-0.756 0.749-0.813c27.047-29.356 33.876-70.34 17.823-106.957-15.942-36.366-50.416-58.957-89.968-58.957H163.604c-38.83 0-73.043 22.012-89.29 57.444-16.361 35.683-10.632 76.301 14.949 106.004l0.97 1.126 280.311 266.85 2.032 312.074c0.107 16.502 13.517 29.805 29.995 29.805l0.2-0.001c16.568-0.107 29.912-13.626 29.804-30.194l-2.198-337.564-296.478-282.241c-9.526-11.758-11.426-26.933-5.044-40.851 6.446-14.059 19.437-22.452 34.75-22.452h624.828c15.6 0 28.69 8.616 35.017 23.047 6.31 14.391 3.924 29.831-6.354 41.497l-304.13 284.832 1.302 458.63c0.047 16.54 13.469 29.916 29.998 29.915h0.087c16.568-0.047 29.962-13.517 29.915-30.085L573.04 502.36l285.98-267.836z"
-        p-id="1149"
-      ></path>
-      <path
-        d="M657.265 595.287c0 16.498 13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.498 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997H687.262c-16.498 0-29.997 13.499-29.997 29.997zM931.159 734.169H687.262c-16.498 0-29.997 13.499-29.997 29.997s13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.499 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997zM931.159 903.047H687.262c-16.498 0-29.997 13.499-29.997 29.997s13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.499 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997z"
-        p-id="1150"
-      ></path>
-    </svg> -->
-    <el-popover placement="boottom" width="300" trigger="click">
-      <!-- <el-button slot="reference">筛选</el-button> -->
-      <el-button slot="reference" @click="visible = !visible">筛选</el-button>
-      <p class="title-text">参课和完课筛选</p>
-      <p>选择课程</p>
-      <el-autocomplete
-        class="inline-input"
-        v-model="state2"
-        :fetch-suggestions="querySearch"
-        placeholder="请输入内容"
-        :trigger-on-focus="false"
-        @select="handleSelect"
-      ></el-autocomplete>
-      <p>状态</p>
-      <el-checkbox-group
-        class="check-states-box"
-        v-model="checkedCities"
-        @change="handleCheckedCitiesChange"
-      >
-        <el-checkbox
-          class="check-states"
-          v-for="state in states"
-          :label="state"
-          :key="state"
-          >{{ state }}</el-checkbox
+    <div v-if="tables.tabs == 3">
+      <el-popover placement="bottom" width="300" trigger="click">
+        <!-- <el-button slot="reference">筛选</el-button> -->
+
+        <el-button
+          class="screen-text"
+          slot="reference"
+          @click="visible = !visible"
         >
-      </el-checkbox-group>
-      <p>参课情况</p>
-      <el-checkbox-group
-        v-model="checkedCities"
-        @change="handleCheckedCitiesChange"
-      >
-        <el-checkbox v-for="friend in friends" :label="friend" :key="friend">{{
-          friend
-        }}</el-checkbox>
-      </el-checkbox-group>
-      <p>完课情况</p>
-      <el-checkbox-group
-        v-model="checkedCities"
-        @change="handleCheckedCitiesChange"
-      >
-        <el-checkbox v-for="group in groups" :label="group" :key="group">{{
-          group
-        }}</el-checkbox>
-      </el-checkbox-group>
-      <div class="check-button">
-        <el-button @click="cancel" size="small">清空</el-button>
-        <el-button size="medium" type="primary">过滤</el-button>
-        <el-button size="medium" type="primary">取消</el-button>
-      </div>
-    </el-popover>
+          <span>
+            <svg
+              t="1586260816244"
+              class="icon-filter"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="1148"
+              width="200"
+              height="200"
+            >
+              <path
+                d="M859.02 234.524l0.808-0.756 0.749-0.813c27.047-29.356 33.876-70.34 17.823-106.957-15.942-36.366-50.416-58.957-89.968-58.957H163.604c-38.83 0-73.043 22.012-89.29 57.444-16.361 35.683-10.632 76.301 14.949 106.004l0.97 1.126 280.311 266.85 2.032 312.074c0.107 16.502 13.517 29.805 29.995 29.805l0.2-0.001c16.568-0.107 29.912-13.626 29.804-30.194l-2.198-337.564-296.478-282.241c-9.526-11.758-11.426-26.933-5.044-40.851 6.446-14.059 19.437-22.452 34.75-22.452h624.828c15.6 0 28.69 8.616 35.017 23.047 6.31 14.391 3.924 29.831-6.354 41.497l-304.13 284.832 1.302 458.63c0.047 16.54 13.469 29.916 29.998 29.915h0.087c16.568-0.047 29.962-13.517 29.915-30.085L573.04 502.36l285.98-267.836z"
+                p-id="1149"
+              ></path>
+              <path
+                d="M657.265 595.287c0 16.498 13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.498 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997H687.262c-16.498 0-29.997 13.499-29.997 29.997zM931.159 734.169H687.262c-16.498 0-29.997 13.499-29.997 29.997s13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.499 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997zM931.159 903.047H687.262c-16.498 0-29.997 13.499-29.997 29.997s13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.499 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997z"
+                p-id="1150"
+              ></path>
+            </svg>
+          </span>
+          筛选</el-button
+        >
+        <p class="title-text">参课和完课筛选</p>
+        <p>选择课程</p>
+        <el-autocomplete
+          :clearable="true"
+          class="inline-input"
+          v-model="state2"
+          :fetch-suggestions="querySearch"
+          placeholder="请输入内容"
+          :trigger-on-focus="false"
+          @select="handleSelect"
+        ></el-autocomplete>
+        <p>状态</p>
+        <el-checkbox-group
+          class="check-states-box"
+          v-model="checkedCities"
+          @change="handleCheckedCitiesChange"
+        >
+          <el-checkbox
+            class="check-states"
+            v-for="state in states"
+            :label="state"
+            :key="state"
+            >{{ state }}</el-checkbox
+          >
+        </el-checkbox-group>
+        <p>参课情况</p>
+        <el-checkbox-group
+          v-model="checkedCities"
+          @change="handleCheckedCitiesChange"
+        >
+          <el-checkbox
+            v-for="friend in friends"
+            :label="friend"
+            :key="friend"
+            >{{ friend }}</el-checkbox
+          >
+        </el-checkbox-group>
+        <p>完课情况</p>
+        <el-checkbox-group
+          v-model="checkedCities"
+          @change="handleCheckedCitiesChange"
+        >
+          <el-checkbox v-for="group in groups" :label="group" :key="group">{{
+            group
+          }}</el-checkbox>
+        </el-checkbox-group>
+        <div class="check-button">
+          <el-button @click="cancel" size="small">清空</el-button>
+          <el-button size="medium" type="primary">过滤</el-button>
+          <el-button size="medium" type="primary">取消</el-button>
+        </div>
+      </el-popover>
+    </div>
+    <div v-if="tables.tabs == 4">
+      <el-popover placement="bottom" width="300" trigger="click">
+        <!-- <el-button slot="reference">筛选</el-button> -->
+
+        <el-button
+          class="screen-text"
+          slot="reference"
+          @click="visible = !visible"
+        >
+          <span>
+            <svg
+              t="1586260816244"
+              class="icon-filter"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="1148"
+              width="200"
+              height="200"
+            >
+              <path
+                d="M859.02 234.524l0.808-0.756 0.749-0.813c27.047-29.356 33.876-70.34 17.823-106.957-15.942-36.366-50.416-58.957-89.968-58.957H163.604c-38.83 0-73.043 22.012-89.29 57.444-16.361 35.683-10.632 76.301 14.949 106.004l0.97 1.126 280.311 266.85 2.032 312.074c0.107 16.502 13.517 29.805 29.995 29.805l0.2-0.001c16.568-0.107 29.912-13.626 29.804-30.194l-2.198-337.564-296.478-282.241c-9.526-11.758-11.426-26.933-5.044-40.851 6.446-14.059 19.437-22.452 34.75-22.452h624.828c15.6 0 28.69 8.616 35.017 23.047 6.31 14.391 3.924 29.831-6.354 41.497l-304.13 284.832 1.302 458.63c0.047 16.54 13.469 29.916 29.998 29.915h0.087c16.568-0.047 29.962-13.517 29.915-30.085L573.04 502.36l285.98-267.836z"
+                p-id="1149"
+              ></path>
+              <path
+                d="M657.265 595.287c0 16.498 13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.498 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997H687.262c-16.498 0-29.997 13.499-29.997 29.997zM931.159 734.169H687.262c-16.498 0-29.997 13.499-29.997 29.997s13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.499 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997zM931.159 903.047H687.262c-16.498 0-29.997 13.499-29.997 29.997s13.499 29.997 29.997 29.997h243.897c16.498 0 29.997-13.499 29.997-29.997 0-16.498-13.499-29.997-29.997-29.997z"
+                p-id="1150"
+              ></path>
+            </svg>
+          </span>
+          筛选</el-button
+        >
+        <p class="title-text">作品及点评筛选</p>
+        <p>选择课程</p>
+        <el-autocomplete
+          :clearable="true"
+          class="inline-input"
+          v-model="state2"
+          :fetch-suggestions="querySearch"
+          placeholder="请输入内容"
+          :trigger-on-focus="false"
+          @select="handleSelect"
+        ></el-autocomplete>
+        <p>状态</p>
+        <el-checkbox-group
+          class="check-states-box"
+          v-model="checkedCities"
+          @change="handleCheckedCitiesChange"
+        >
+          <el-checkbox
+            class="check-states"
+            v-for="state in states"
+            :label="state"
+            :key="state"
+            >{{ state }}</el-checkbox
+          >
+        </el-checkbox-group>
+        <p>参课情况</p>
+        <el-checkbox-group
+          v-model="checkedCities"
+          @change="handleCheckedCitiesChange"
+        >
+          <el-checkbox
+            v-for="friend in friends"
+            :label="friend"
+            :key="friend"
+            >{{ friend }}</el-checkbox
+          >
+        </el-checkbox-group>
+        <p>完课情况</p>
+        <el-checkbox-group
+          v-model="checkedCities"
+          @change="handleCheckedCitiesChange"
+        >
+          <el-checkbox v-for="group in groups" :label="group" :key="group">{{
+            group
+          }}</el-checkbox>
+        </el-checkbox-group>
+        <div class="check-button">
+          <el-button @click="cancel" size="small">清空</el-button>
+          <el-button size="medium" type="primary">过滤</el-button>
+          <el-button size="medium" type="primary">取消</el-button>
+        </div>
+      </el-popover>
+    </div>
   </div>
 </template>
 <script>
@@ -260,6 +360,18 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.screen-text {
+  border: none;
+}
+// .screen-text:active {
+//   background: #fff;
+//   border: none;
+// }
+.icon-filter {
+  font-size: 24px;
+  height: 20px;
+  width: 20px;
+}
 .title-text {
   font-weight: 400;
   font-size: 22px;
