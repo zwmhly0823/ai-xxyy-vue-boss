f5da691089422613072ba884b78701dd36dc1a6a
YangJiyong <yangjiyong@meishubao.com>
2020-04-13 20:58:46 +0800

    update: 生成完课圈 每页70个

--- src/pages/studentTeam/components/TabPane/DetailsTab.vue
+++ src/pages/studentTeam/components/TabPane/DetailsTab.vue
@@ -663,7 +663,7 @@ export default {
         const childLastData = []
         if (res.data.getStuComRankingList) {
           const stuArrLength = res.data.getStuComRankingList.length
-          const createDefineNum = 20
+          const createDefineNum = 70
           const arevNum = Math.ceil(
             stuArrLength / Math.ceil(stuArrLength / createDefineNum)
           )
