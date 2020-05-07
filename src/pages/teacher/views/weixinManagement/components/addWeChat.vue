b3d68e6ebea065d669fc6ca4c846cd3626640c61
panjian <panjian@meishubao.com>
2020-05-07 18:23:51 +0800

    fix: 微信管理 添加微信 关联老师 修改

--- src/pages/teacher/views/weixinManagement/components/addWeChat.vue
+++ src/pages/teacher/views/weixinManagement/components/addWeChat.vue
@@ -3,8 +3,8 @@
  * @version: 
  * @Author: panjian
  * @Date: 2020-04-14 15:15:31
- * @LastEditors: Shentong
- * @LastEditTime: 2020-04-22 15:25:43
+ * @LastEditors: panjian
+ * @LastEditTime: 2020-05-07 18:17:36
  -->
 <template>
   <div>
@@ -207,23 +207,27 @@ export default {
         setTimeout(() => {
           this.loading = false
           if (this.ruleForm.associatedTeacher) {
-            this.TeacherListvalue = `{"department_id": ${this.ruleForm.associatedTeacher}}`
+            this.TeacherListvalue = {
+              department_id: `${this.ruleForm.associatedTeacher}`
+            }
           }
-          this.$http.Teacher.TeacherList(this.TeacherListvalue).then((res) => {
-            const data = res.data.TeacherList
-            const _data = []
-            data.forEach((res) => {
-              _data.push({
-                value: res.id,
-                label: res.realname
+          this.$http.Teacher.TeacherList(this.TeacherListvalue, query).then(
+            (res) => {
+              const data = res.data.TeacherListEx
+              const _data = []
+              data.forEach((res) => {
+                _data.push({
+                  value: res.id,
+                  label: res.realname
+                })
               })
-            })
-            this.regionOptionsList = _data.filter((item) => {
-              return query
-                ? item.label.toLowerCase().indexOf(query.toLowerCase()) > -1
-                : item
-            })
-          })
+              this.regionOptionsList = _data.filter((item) => {
+                return query
+                  ? item.label.toLowerCase().indexOf(query.toLowerCase()) > -1
+                  : item
+              })
+            }
+          )
         }, 200)
       } else {
         this.regionOptionsList = []
