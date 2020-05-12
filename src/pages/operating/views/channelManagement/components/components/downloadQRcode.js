52e858928205981b24646471c80a941d2719778f
panjian <panjian@meishubao.com>
2020-05-12 11:44:08 +0800

    fix: 删除多余代码

--- src/pages/operating/views/channelManagement/components/components/downloadQRcode.js
+++ src/pages/operating/views/channelManagement/components/components/downloadQRcode.js
@@ -4,7 +4,7 @@
  * @Author: panjian
  * @Date: 2020-05-09 16:02:33
  * @LastEditors: panjian
- * @LastEditTime: 2020-05-12 11:32:01
+ * @LastEditTime: 2020-05-12 11:43:30
  */
 
 import JSZip from 'jszip'
@@ -46,14 +46,12 @@ export function downImgAll(imgListName, imgUrlList) {
     const imgNameList = imgListName
     const arr = imgUrlList
     // let arr = ['./img1.png', './img2.png', './img3.png']
-    console.log(arr)
 
     for (let i = 0; i < arr.length; i++) {
       const image = new Image()
       // 解决跨域 Canvas 污染问题
       image.setAttribute('crossOrigin', 'anonymous')
       const downLoadImg = function(url) {
-        if (!url) return
         // const canvas = document.createElement('canvas')
         // canvas.width = image.width
         // canvas.height = image.height
@@ -86,60 +84,3 @@ export function downImgAll(imgListName, imgUrlList) {
     }
   })
 }
-const FunLib = {
-  // 图片打包下载
-  download: function(images) {
-    FunLib.packageImages(images)
-  },
-  // 打包压缩图片
-  packageImages: function(imgs) {
-    var imgBase64 = []
-    var imageSuffix = [] // 图片后缀
-    var zip = new JSZip()
-    var img = zip.folder('images')
-
-    for (var i = 0; i < imgs.length; i++) {
-      var src = imgs[i].url
-      var suffix = src.substring(src.lastIndexOf('.'))
-      imageSuffix.push(suffix)
-      FunLib.getBase64(imgs[i].url).then(
-        function(base64) {
-          imgBase64.push(base64.substring(22))
-          if (imgs.length === imgBase64.length) {
-            for (var i = 0; i < imgs.length; i++) {
-              img.file(imgs[i].name + imageSuffix[i], imgBase64[i], {
-                base64: true
-              })
-            }
-            zip.generateAsync({ type: 'blob' }).then(function(content) {
-              FileSaver.saveAs(content, 'images.zip')
-            })
-          }
-        },
-        function(err) {
-          console.log(err)
-        }
-      )
-    }
-  },
-  // 传入图片路径，返回base64
-  getBase64: function(img) {
-    var image = new Image()
-    image.crossOrigin = 'Anonymous'
-    image.src = img
-    var deferred = new Promise()
-    if (img) {
-      image.onload = function() {
-        var canvas = document.createElement('canvas')
-        canvas.width = image.width
-        canvas.height = image.height
-        var ctx = canvas.getContext('2d')
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height)
-        var dataURL = canvas.toDataURL()
-        deferred.resolve(dataURL)
-      }
-      return deferred.promise()
-    }
-  }
-}
-export default FunLib
