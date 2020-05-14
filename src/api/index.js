a9edeaf49045b4d927aa9ae97ac9433911efcf04
lukun <lukun@meishubao.com>
2020-05-14 15:12:24 +0800

    fix:物流页面导入导出axios替换

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,8 @@
  * @version:
  * @Author: shentong
  * @Date: 2020-03-13 14:38:28
- * @LastEditors: Shentong
- * @LastEditTime: 2020-04-24 17:43:08
+ * @LastEditors: Lukun
+ * @LastEditTime: 2020-05-14 14:40:13
  */
 import Login from './modules/Login'
 import User from './modules/User' // 用户接口
@@ -17,6 +17,7 @@ import Department from './modules/Department' // 组织机构接口
 import Product from './modules/Product' // 产品相关接口
 import Teacher from './modules/Teacher' // 老师模块
 import Statistics from './modules/Statistics' // 统计模块
+import DownloadExcel from './modules/DownloadExcel' // 统计模块
 
 export default Object.assign(
   {},
@@ -30,5 +31,6 @@ export default Object.assign(
   { Department },
   { Product },
   { Teacher },
-  { Statistics }
+  { Statistics },
+  { DownloadExcel }
 )
---
+++ src/api/modules/DownloadExcel.js
@@ -0,0 +1,46 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Lukun
+ * @Date: 2020-05-14 14:31:42
+ * @LastEditors: Lukun
+ * @LastEditTime: 2020-05-14 15:09:33
+ */
+import axios from '../axios'
+import { getToken } from '@/utils/auth'
+
+const getHeaders = () => {
+  const token = getToken() || ''
+  const headers = {
+    'Content-Type': 'application/json;charset=UTF-8'
+  }
+  if (token) {
+    headers.Authorization = 'Bearer ' + token
+  }
+  return headers
+}
+const judgeToken = () => {
+  const token = getHeaders().Authorization
+  const needToken = location.href.indexOf('login') === -1
+
+  if (needToken && !token) {
+    // location.href = `${baseUrl}login/#/`
+    location.href = `/login/#/`
+    return 0
+  }
+  return 1
+}
+export default {
+  // 物流页面导出物流Excle文件
+  exportExpress(params) {
+    if (judgeToken()) {
+      return axios({
+        method: 'POST',
+        url: '/data/search/m1/v1/search/common/export',
+        responseType: 'blob',
+        headers: getHeaders(),
+        params
+      })
+    }
+  }
+}
