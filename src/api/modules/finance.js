897ad0b841749644c9bed3c44fc20ad813cfdf71
liukun <liukun@meishubao.com>
2020-05-29 18:42:47 +0800

    头部注释日期冲突

--- src/api/modules/finance.js
+++ src/api/modules/finance.js
@@ -4,11 +4,7 @@
  * @Author: liukun
  * @Date: 2020-05-22 12:39:14
  * @LastEditors: liukun
-<<<<<<< HEAD
- * @LastEditTime: 2020-05-29 17:07:04
-=======
- * @LastEditTime: 2020-05-26 23:41:46
->>>>>>> ee06589a0e4711db24cfd7d6efe72eaae0e0cd2e
+ * @LastEditTime: 2020-05-29 18:41:29
  */
 
 import axios from '../axiosConfig'
