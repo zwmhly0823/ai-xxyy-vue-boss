a1e0485555ce19bdc6716ed891bd3f6a0cb47831
panjian <panjian@meishubao.com>
2020-06-08 16:35:43 +0800

    fix: 修改系统标签参数

--- src/pages/operating/views/labelManage/components/systemLabels/index.vue
+++ src/pages/operating/views/labelManage/components/systemLabels/index.vue
@@ -4,7 +4,7 @@
  * @Author: panjian
  * @Date: 2020-05-30 18:37:24
  * @LastEditors: panjian
- * @LastEditTime: 2020-06-08 16:06:10
+ * @LastEditTime: 2020-06-08 16:34:55
 -->
 <template>
   <div class="system-label">
@@ -70,7 +70,7 @@ export default {
   methods: {
     getFindLabelByPage() {
       const params = {
-        tyep: 'SYSTEM',
+        type: 'SYSTEM',
         name: (this.tableName = this.tableName ? this.tableName : ''),
         labelAttr: (this.tableAttribute = this.tableAttribute
           ? this.tableAttribute
