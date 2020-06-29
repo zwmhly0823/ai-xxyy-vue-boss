b668f5b2c0462279768c1a8bdfa62c9e5149b18b
shentong <shentong@meishubao.com>
2020-06-29 20:28:33 +0800

    new-sop

--- src/pages/community/views/newSopTmp/index.vue
+++ src/pages/community/views/newSopTmp/index.vue
@@ -4,14 +4,55 @@
  * @Author: Shentong
  * @Date: 2020-06-29 16:50:58
  * @LastEditors: Shentong
- * @LastEditTime: 2020-06-29 18:52:01
+ * @LastEditTime: 2020-06-29 20:27:11
 -->
 <template>
   <el-row type="flex" class="new-sop app-main">
     <el-col class="new-sop-container">
       <div class="scroll-container">
         <el-scrollbar wrap-class="scrollbar-wrapper">
-          <div>new-sop</div>
+          <el-card class="header">
+            <div class="tip">新建SOP模板</div>
+            <el-form
+              :model="sopFrom"
+              ref="sopFrom"
+              label-width="100px"
+              size="mini"
+              class="sop-form"
+            >
+              <el-form-item
+                prop="name"
+                label="模板名称"
+                style="width:400px;"
+                :rules="[
+                  {
+                    required: true,
+                    message: '请输入邮箱地址',
+                    trigger: 'blur'
+                  },
+                  {
+                    type: 'name',
+                    message: '请输入正确的邮箱地址',
+                    trigger: ['blur', 'change']
+                  }
+                ]"
+              >
+                <el-input
+                  v-model="sopFrom.name"
+                  placeholder="请输入模板名称"
+                ></el-input>
+              </el-form-item>
+              <el-form-item label="模板状态" prop="status">
+                <el-radio-group v-model="sopFrom.status">
+                  <el-radio :label="0">启用</el-radio>
+                  <el-radio :label="1">禁用</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-form>
+          </el-card>
+          <el-card class="section">
+            section
+          </el-card>
         </el-scrollbar>
       </div>
     </el-col>
@@ -20,7 +61,12 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      sopFrom: {
+        name: '',
+        status: 0
+      }
+    }
   },
   components: {},
   computed: {},
@@ -32,12 +78,10 @@ export default {
   .new-sop-container {
     position: relative;
     margin: 10px;
-    background: #fff;
     overflow-x: hidden;
     flex: 1;
     margin: 10px;
     .scroll-container {
-      background: white;
       height: 100%;
       display: flex;
       flex-direction: column;
@@ -46,6 +90,15 @@ export default {
       }
       .el-scrollbar {
         flex: 1;
+      }
+      .header {
+        margin-bottom: 10px;
+        .tip {
+          margin-bottom: 20px;
+        }
+        .sop-form {
+          font-size: 12px;
+        }
       }
     }
   }
