530c275f2ccbae710cc630f2620c75774a1b43bf
shentong <shentong@meishubao.com>
2020-06-30 15:25:09 +0800

    init-sop

--- src/pages/community/views/newSopTmp/index.vue
+++ src/pages/community/views/newSopTmp/index.vue
@@ -4,7 +4,7 @@
  * @Author: Shentong
  * @Date: 2020-06-29 16:50:58
  * @LastEditors: Shentong
- * @LastEditTime: 2020-06-29 20:27:11
+ * @LastEditTime: 2020-06-30 14:54:11
 -->
 <template>
   <el-row type="flex" class="new-sop app-main">
@@ -23,7 +23,7 @@
               <el-form-item
                 prop="name"
                 label="模板名称"
-                style="width:400px;"
+                style="width:320px;"
                 :rules="[
                   {
                     required: true,
@@ -51,7 +51,34 @@
             </el-form>
           </el-card>
           <el-card class="section">
-            section
+            <div class="task-container">
+              <div class="task-group">
+                <div class="item">
+                  <span>第一天任务</span>
+                  <i>×</i>
+                </div>
+              </div>
+              <div class="task-detail">
+                <div class="label">发送时间</div>
+                <div class="send-time">
+                  <div class="day">第<span>1</span>天</div>
+                  <el-time-picker
+                    size="mini"
+                    is-range
+                    v-model="sendTime"
+                    range-separator="至"
+                    start-placeholder="开始时间"
+                    end-placeholder="结束时间"
+                    placeholder="选择时间范围"
+                    class="time-picker"
+                  >
+                  </el-time-picker>
+                  <span class="tip">第一条信息在时间段内随机发送</span>
+                </div>
+                <div class="label content">发送内容</div>
+                <div class="send-content"></div>
+              </div>
+            </div>
           </el-card>
         </el-scrollbar>
       </div>
@@ -65,7 +92,8 @@ export default {
       sopFrom: {
         name: '',
         status: 0
-      }
+      },
+      sendTime: [new Date(2019, 9, 10, 8, 40), new Date(2019, 9, 10, 9, 40)]
     }
   },
   components: {},
@@ -98,6 +126,68 @@ export default {
         }
         .sop-form {
           font-size: 12px;
+        }
+      }
+      .section {
+        .task-container {
+          display: flex;
+          .task-group {
+            width: 180px;
+            margin-right: 20px;
+            .item {
+              width: 100%;
+              border: 1px solid #dcdfe6;
+              color: #2a75ed;
+              height: 30px;
+              border-radius: 5px;
+              display: flex;
+              align-items: center;
+              padding: 0 10px;
+              justify-content: center;
+              transition: box-shadow 0.2s;
+              position: relative;
+              i {
+                cursor: pointer;
+                height: 100%;
+                width: 30px;
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+                position: absolute;
+                right: 10px;
+                top: 0;
+              }
+              &:hover {
+                box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+              }
+            }
+          }
+          .task-detail {
+            flex: 1;
+            padding: 10px;
+            border-radius: 3px;
+            background: #ebeef3;
+            .label {
+              margin-bottom: 10px;
+              &.content {
+                margin-top: 40px;
+              }
+            }
+            .send-time {
+              display: flex;
+              align-items: center;
+              .day {
+                span {
+                  color: #2a75ed;
+                  padding: 0 20px;
+                  font-weight: 500;
+                }
+              }
+              .time-picker {
+                margin: 0 15px;
+              }
+            }
+          }
         }
       }
     }
