626bf9ee7b1321e566207a14d816f07e34114964
hexin <hexin@meishubao.com>
2020-07-02 21:28:40 +0800

    feat:物流详情查询条件更新

--- src/pages/trading/views/components/expressDetail.vue
+++ src/pages/trading/views/components/expressDetail.vue
@@ -55,7 +55,7 @@
           </div>
           <div class="horizontal-line"></div>
           <div class="waitFor" v-if="waitFor">快递待揽收</div>
-          <el-timeline v-if="activities" class="right-timeline">
+          <el-timeline v-if="!isShowNew" class="right-timeline">
             <el-timeline-item
               v-for="(value, index) in activities"
               :key="index"
@@ -74,7 +74,7 @@
               </div>
             </el-timeline-item>
           </el-timeline>
-          <el-timeline class="right-timeline">
+          <el-timeline v-if="isShowNew" class="right-timeline">
             <el-timeline-item
               v-for="(items, index) in expressDetailsInfo"
               :key="index"
@@ -191,21 +191,32 @@ export default {
       orderInformation: [],
       expressInformation: [],
       activities: [],
-      expressDetailsInfo: [],
+      expressDetailsInfo: [], // 从物流中台获取的物流信息
       isActive: 0,
       leftRow: [],
       orderId: '',
-      expressNu: '' // 物流单号
+      expressNu: '', // 物流单号
+      isShowNew: true // 是否展示新物流中台获取的物流数据
     }
   },
   watch: {
     transferExpress(val) {
       this.waitFor = false
       this.activities = []
+      this.expressDetailsInfo = []
       this.expressInformation = val
       this.expressNu = this.expressInformation.express_nu
-      this.getExpressDetails('WL11127200499586')
-      // this.expressList(this.expressNu, val.express_status)
+      const timeNodes = new Date('2020-07-01').getTime()
+      // 2020-07-01之后且center_express_id 不等于0 且不是中通快递 取中台数据
+      if (
+        +val.center_ctime > timeNodes &&
+        val.center_express_id !== '0' &&
+        String(val.express_company).indexOf('中通') === -1
+      ) {
+        this.getExpressDetails(this.expressNu)
+      } else {
+        this.expressList(this.expressNu, val.express_status)
+      }
     },
     order_id(val) {
       this.waitFor = false
@@ -260,7 +271,6 @@ export default {
           this.editResult = true
           this.isShowEditStatus = false
           this.$message.success('状态修改成功')
-          console.log(res)
         } else {
           this.editResult = false
           this.$message.error('状态修改失败')
@@ -291,7 +301,17 @@ export default {
     getexpressInformation(item, i) {
       this.isActive = i
       this.expressInformation = item
-      this.expressList(item.express_nu, item.express_status)
+      const timeNodes = new Date('2020-07-01').getTime()
+      // 2020-07-01之后且center_express_id 不等于0 且不是中通快递 取中台数据
+      if (
+        +item.center_ctime > timeNodes &&
+        item.center_express_id !== '0' &&
+        String(item.express_company).indexOf('中通') === -1
+      ) {
+        this.getExpressDetails(item.express_nu)
+      } else {
+        this.expressList(item.express_nu, item.express_status)
+      }
     },
     handleClose() {
       this.drawer = false
@@ -311,6 +331,8 @@ export default {
                       express_company
                       express_nu
                       express_status
+                      center_ctime
+                      center_express_id
                   }
                 }`
         })
@@ -324,11 +346,19 @@ export default {
       this.$http.Express.getExpressDetails({ expressNo })
         .catch((err) => console.log(err))
         .then((res) => {
-          this.expressDetailsInfo = res.payload[0].data
+          this.isShowNew = true
+          if (res.payload && res.payload[0].data.length > 0) {
+            this.waitFor = false
+            this.expressDetailsInfo = res.payload[0].data
+          } else {
+            this.expressDetailsInfo = []
+            this.waitFor = true
+          }
         })
     },
     // 物流列表信息
     expressList(id, expressStatus) {
+      this.isShowNew = false
       const lastData = {}
       // 如果是京东物流 且物流状态 expressStatus 为2 请求京东物流接口
       if (id.toString().indexOf('JD') > -1 && expressStatus === '2') {
@@ -336,7 +366,7 @@ export default {
           const tempData =
             (jdRes && jdRes.payload && jdRes.payload[0].data) || []
           if (tempData.length > 0) {
-            // lastData.begin = []
+            this.waitFor = false
             tempData.forEach((item, index) => {
               item.status = item.opeTitle
               lastData[index] = []
