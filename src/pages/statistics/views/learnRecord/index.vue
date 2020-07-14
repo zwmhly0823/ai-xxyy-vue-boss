898a8a000938a12ec4522325f8a77178f6a2af3e
zhangjianwen <zhangjianwen@meishubao.com>
2020-07-14 16:23:03 +0800

    style:字体大小

--- src/pages/statistics/views/learnRecord/index.vue
+++ src/pages/statistics/views/learnRecord/index.vue
@@ -4,7 +4,7 @@
  * @Author: zhangjianwen
  * @Date: 2020-07-09 15:02:59
  * @LastEditors: zhangjianwen
- * @LastEditTime: 2020-07-14 15:47:23
+ * @LastEditTime: 2020-07-14 16:18:54
 -->
 <template>
   <div class="learn-record">
@@ -305,7 +305,7 @@ export default {
     .content-word {
       padding: 14px;
       p {
-        font-size: 12px;
+        font-size: 14px;
         height: 20px;
       }
     }
