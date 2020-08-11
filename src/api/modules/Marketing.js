5bb3072e9d57463f8356e510c8f2d93f6ba01487
杨吉勇 <yangjiyong@meishubao.com>
2020-08-11 19:01:34 +0800

    update: 兑换记录增加按状态排序条件

--- src/api/modules/Marketing.js
+++ src/api/modules/Marketing.js
@@ -4,7 +4,7 @@
  * @Author: YangJiyong
  * @Date: 2020-08-07 16:39:06
  * @LastEditors: YangJiyong
- * @LastEditTime: 2020-08-10 21:18:05
+ * @LastEditTime: 2020-08-11 18:59:22
  */
 import axios from '../axiosConfig'
 
@@ -126,7 +126,9 @@ export default {
         : JSON.stringify(JSON.stringify(params))
     const sortObj = sort
       ? JSON.stringify(
-          JSON.stringify(Object.assign(sort, { use_date: 'desc' }))
+          JSON.stringify(
+            Object.assign(sort, { status: 'desc', use_date: 'desc' })
+          )
         )
       : `""`
     return axios.post('/graphql/v1/toss', {
