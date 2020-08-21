33b0605d26896868da19233f19311c5422305771
shentong <shentong@meishubao.com>
2020-08-21 15:24:34 +0800

    招生排期init

---
+++ src/pages/msb-writing/views/Operating/index.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Descripttion: 
+ * @version: 1.0.0
+ * @Author: YangJiyong
+ * @Date: 2020-08-19 22:11:50
+ * @LastEditors: Shentong
+ * @LastEditTime: 2020-08-21 15:08:03
+-->
+<template>
+  <div>美术宝写字-招生排期</div>
+</template>
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
