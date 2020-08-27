f96ca5998e61ef23d9fa66f767cb001fa1c2b121
shentong <shentong@meishubao.com>
2020-08-27 20:56:27 +0800

    update：新增Teacher模块接口

--- src/api/index_write_app.js
+++ src/api/index_write_app.js
@@ -4,7 +4,7 @@
  * @Author: YangJiyong
  * @Date: 2020-08-19 20:42:54
  * @LastEditors: Shentong
- * @LastEditTime: 2020-08-26 15:51:58
+ * @LastEditTime: 2020-08-27 20:50:49
  */
 
 import User from './modules/write_app/User' // 用户接口
@@ -12,11 +12,13 @@ import Operating from './modules/write_app/Operating' // 运营中心
 import Order from './modules/write_app/Order' // 订单接口
 import Team from './modules/write_app/Team' // 班级接口
 import DownloadExcel from './modules/DownloadExcel' // 统计模块
+import Teacher from './modules/write_app/Teacher' // 老师模块API
 export default Object.assign(
   {},
   { User },
   { Operating },
   { Order },
   { DownloadExcel },
-  { Team }
+  { Team },
+  { Teacher }
 )
