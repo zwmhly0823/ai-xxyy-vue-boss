74c909a8841fb5dc811507153e22bec5c8be51ec
lfnb <lfnb@msn.cn>
2021-01-19 15:38:54 +0800

    feat: 新增活动

--- src/api/modules/Active.js
+++ src/api/modules/Active.js
@@ -2,7 +2,7 @@ import axios from '../axiosConfig'
 
 export default {
   test(params) {
-    return axios.post('/v1/mongo/search',{
+    return axios.post('/data/mongo/m1/v1/mongo/search/common',{
       tableName: 'a_account',
       ...params
     })
