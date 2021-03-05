aea20f353d84d836d4221ea30fe27a260587d526
zhaowenyu <zhaowenyu01@meishubao.com>
2021-03-05 21:43:30 +0800

    fix: wuliu

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,5 +1,6 @@
 export default {
   OSS_IMG_BASE_URL: 'https://xxyy-kczzht.oss-cn-hangzhou.aliyuncs.com',
+  OSS_IMG_BASE_URL_ONLINE: 'https://ai-xxyy-backend-online.oss-cn-hangzhou.aliyuncs.com',
   OSS_IMG_BASE_URL_2: 'https://s2.xiaoxiongmeishu.com',
   DEFAUTL_URL: 'https://msb-ai.meixiu.mobi/ai-pm/static/touxiang.png?', // 默认的图片路径
   PHOTO_BORDER: 'https://s1.meixiu.mobi/image/border.png/', // 作品详情页相框
