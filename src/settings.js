15e2eaeb742b4559e454823b7fe649158430b80d
“zhaowenming” <zhaowenming@meishubao.com>
2021-03-23 15:28:00 +0800

    fix: publish version

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-23 11:38:58
+ * @LastEditTime: 2021-03-23 15:27:32
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /ai-xxyy-vue-boss/src/settings.js
+ */
 module.exports = {
   title: '小熊音乐BOSS',
-  version: '1.2.4',
+  version: '1.2.5',
 
   /**
    * @type {boolean} true | false
