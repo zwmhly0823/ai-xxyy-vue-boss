72ed8d13d89465ca032a7eee05903a3953a13cca
“zhaowenming” <zhaowenming@meishubao.com>
2021-03-23 14:53:53 +0800

    feat: tags

--- src/pages/music_app/views/users/components/trial/tags/tagDetail.vue
+++ src/pages/music_app/views/users/components/trial/tags/tagDetail.vue
@@ -467,14 +467,14 @@ export default {
       const labelNames = []
       console.log(this.rightDepartSysTagList)
       console.log(this.rightPersonSysTagList)
-      // Object.keys(this.rightDepartSysTagList).forEach((key) => {
-      //   this.rightDepartSysTagList[key].forEach((item) => {
-      //     if (item.active) {
-      //       labelIds.push(item.labelInfoId)
-      //       labelNames.push(item.name)
-      //     }
-      //   })
-      // })
+      Object.keys(this.rightDepartSysTagList).forEach((key) => {
+        this.rightDepartSysTagList[key].forEach((item) => {
+          if (item.active) {
+            labelIds.push(item.labelInfoId)
+            labelNames.push(item.name)
+          }
+        })
+      })
       this.rightPersonSysTagList &&
         this.rightPersonSysTagList.forEach((item) => {
           if (item.active) {
