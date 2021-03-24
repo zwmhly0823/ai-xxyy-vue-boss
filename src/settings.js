5f4adcbb10e4969a667cba80899c9d166bef6724
“zhaowenming” <zhaowenming@meishubao.com>
2021-03-24 15:30:05 +0800

    feat: 版本号

--- src/settings.js
+++ src/settings.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2021-03-23 11:38:58
- * @LastEditTime: 2021-03-23 20:01:46
+ * @LastEditTime: 2021-03-24 15:25:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /ai-xxyy-vue-boss/src/settings.js
  */
 module.exports = {
   title: '小熊音乐BOSS',
-  version: '1.2.6',
+  version: '1.2.7',
 
   /**
    * @type {boolean} true | false
